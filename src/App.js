5fd57b7b90dfdd9cc32731409eff6db367114ab7
OlgaTomilko <67008325+OlgaTomilko@users.noreply.github.com>
2021-03-13 00:29:56 +0300

    Сделано Load more

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import SearchBar from "./components/Searchbar/Searchbar";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Button from "./components/Button/Button";
 import "./App.css";
 
 const ApiKey = "19902573-b9fa82d62327bd625e4b4b636";
@@ -12,17 +13,39 @@ class App extends Component {
   state = {
     images: [],
     page: 1,
+    query: "",
     showModal: false,
   };
 
-  componentDidMount() {}
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  }
 
   onChangeQuery = (query) => {
+    this.setState({ query: query, page: 1, images: [] });
+  };
+
+  fetchImages = () => {
     axios
       .get(
-        `https://pixabay.com/api/?q=${query}&page=${this.state.page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=12`
+        `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=${ApiKey}&image_type=photo&orientation=horizontal&per_page=12`
       )
-      .then((response) => this.setState({ images: response.data.hits }));
+      .then((response) =>
+        this.setState((prevState) => ({
+          images: [...prevState.images, ...response.data.hits],
+          page: prevState.page + 1,
+        }))
+      );
+  };
+
+  onLoadMoreButtonShow = () => {
+    return this.state.images.length > 0;
   };
 
   render() {
@@ -30,6 +53,7 @@ class App extends Component {
       <div className="App">
         <SearchBar onSubmit={this.onChangeQuery} />
         <ImageGallery images={this.state.images} />
+        {this.onLoadMoreButtonShow() && <Button onSubmit={this.fetchImages} />}
       </div>
     );
   }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,1 +1,18 @@
+import React, { Component } from "react";
 import "./Button.css";
+
+class Button extends Component {
+  render() {
+    return (
+      <button
+        type="submit"
+        className="LoadMore-button"
+        onClick={this.props.onSubmit}
+      >
+        Load more
+      </button>
+    );
+  }
+}
+
+export default Button;
